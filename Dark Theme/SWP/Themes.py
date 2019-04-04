c8b536b2e924dfb6970394a6e3d80a5e537dade8
kwenz <konrad.wenz@gmail.com>
2019-04-04 15:54:17 -0400

    Delete Themes.py

--- Dark Theme/SWP/Themes.py
+++
@@ -1,1 +0,0 @@
-Colors={"bg_color":"#464646","plot_color":"#303030","button_bg_color":"#585858","entry_bg_color":"#D6D6D6","label_fg_color":"white","num_color":"#A2E184","inftext_color":"#F09529","on_color":"#A2E184","off_color":"#F45531","ref_laser":"white","laser_1":"#39FF14","laser_2":"#FF37FC","info_color":"#64B7FC"}
